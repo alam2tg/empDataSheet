b2d03f8d99c9fdba21d0804b027929a31e10ce3f
aaron <aaronlamfinance@gmail.com>
2023-08-07 00:57:23 -0700

    Move prompts to index

--- index.js
+++ index.js
@@ -1,3 +1,73 @@
 const inquirer = require('inquirer');
-const question = require('./helpers/questions')
-//create 
+const question = require('./helpers/questions');
+const db = require('./config/connection');
+
+//create initial prompt with list of options. 
+
+function promptOptions() {
+	inquirer
+	.prompt([
+		{
+		type: "list",
+		name: "options",
+		message: "What would you like to do?",
+		choices: ["View All Employees","View All Roles","View All Departments", "Add Employee","Update Employee Role","Add Role","Add Department", "Quit"]
+		}])
+	.then(res => {
+		if (res.options === 'View All Employees') {
+			viewAllEmployees();
+		}
+		if (res.options === 'View All Roles') {
+			viewAllRoles();
+		}
+		if (res.options === 'View All Departments') {
+			viewAllDepartments();
+		}
+		if (res.options === 'Add Employees') {
+		}
+		if (res.options === 'Update Employee Role') {
+		}
+		if (res.options === 'Add Department') {
+		}
+		if (res.options === 'Quit') {
+		}
+	
+	
+	})
+}
+promptOptions();
+
+const viewAllEmployees = ()=> {
+	db.query('SELECT * FROM employees', (err,data) => {
+		if (err) console.log(err)
+		console.table(data)
+		promptOptions();
+	})
+}
+
+const viewAllRoles = ()=> {
+	db.query('SELECT * FROM roles', (err,data) => {
+		if (err) console.log(err)
+		console.table(data)
+		promptOptions();
+	})
+}
+
+const viewAllDepartments = ()=> {
+	db.query('SELECT * FROM department', (err,data) => {
+		if (err) console.log(err)
+		console.table(data)
+		promptOptions();
+	})
+}
+
+//query's to db, if err, logs err, display data with console.table. 
+
+const addNewEmployee = ()=> {
+	db.query('SELECT * FROM employees', (err,data) => {
+		if (err) console.log(err)
+		const employee = data.map(employees => {
+			return { name: employee.first_name + employee.last_name, value: employee.id }
+	})
+	})
+}
